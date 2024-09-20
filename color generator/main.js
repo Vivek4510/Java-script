f733fc6081f830ea47d593e771a6b16b2e847563
Vivek4510 <saivivekreddytummala@gmail.com>
2024-09-20 23:37:46 +0530

    JS

--- color generator/main.js
+++ color generator/main.js
@@ -3,7 +3,8 @@ let txt = document.querySelector("#text");
 let gen = document.querySelector("#generate");
 
 gen.addEventListener("click",() =>{
-    let bg = "#" + Math.floor(Math.random()*16777215).toString(16);
+    let bg = "#" + Math.ceil(Math.random()*16777215).toString(16);
+    console.log(bg);
     txt.innerText = bg;
     main.style.backgroundColor = bg;
 });
